b7f492d3762caaba66db641c24cba089d90c90ee
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-02-06 09:40:03 +0000

    Added Controller

---
+++ scrum-poker-web/src/main/java/uk/laxd/poker/controller/ScrumController.java
@@ -0,0 +1,10 @@
+package uk.laxd.poker.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by lawrence on 06/02/16.
+ */
+@Controller
+public class ScrumController {
+}
