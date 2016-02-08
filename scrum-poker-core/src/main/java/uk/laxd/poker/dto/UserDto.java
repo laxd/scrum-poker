f8d27a29c29139f2d8ed5fea522dff28b5046144
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-02-08 20:17:36 +0000

    Added user and vote DTOs.

---
+++ scrum-poker-core/src/main/java/uk/laxd/poker/dto/UserDto.java
@@ -0,0 +1,16 @@
+package uk.laxd.poker.dto;
+
+/**
+ * Created by lawrence on 08/02/16.
+ */
+public class UserDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ scrum-poker-core/src/main/java/uk/laxd/poker/dto/VoteDto.java
@@ -0,0 +1,16 @@
+package uk.laxd.poker.dto;
+
+/**
+ * Created by lawrence on 08/02/16.
+ */
+public class VoteDto {
+    private String vote;
+
+    public String getVote() {
+        return vote;
+    }
+
+    public void setVote(String vote) {
+        this.vote = vote;
+    }
+}
