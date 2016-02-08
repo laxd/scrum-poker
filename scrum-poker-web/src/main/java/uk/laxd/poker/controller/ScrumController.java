f04179bcb52b6f59e4ea28ddd03037c6976c0a62
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-02-08 15:09:01 +0000

    Added requestmapping for index page

--- scrum-poker-web/src/main/java/uk/laxd/poker/controller/ScrumController.java
+++ scrum-poker-web/src/main/java/uk/laxd/poker/controller/ScrumController.java
@@ -1,10 +1,18 @@
 package uk.laxd.poker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by lawrence on 06/02/16.
  */
 @Controller
 public class ScrumController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
 }
